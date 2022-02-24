d4901363a1a2806dd57a3b5d4c9840bbd641c23e
AliceHLe <94011819+AliceHLe@users.noreply.github.com>
2022-02-24 22:01:37 +0700

    Update cp_task_5.py

--- cp_light_control/cp_task_5.py
+++ cp_light_control/cp_task_5.py
@@ -1,5 +1,6 @@
 #set the LED brightness to be 60000
-#move the LED closer or farther from the light sensor until the sensor reading is between 20000 - 40000. 
+#move the LED closer or farther from the light sensor 
+#until the sensor reading is between 20000 - 40000. 
 
 import time
 import board
@@ -14,6 +15,10 @@ while True:
     #receive analog_in data 
     light = analog_in.value
     #print so that we can move the sensor
-    print(light)
+    print("LED: " + light)
+    if light < 20000:
+        print("Too dim! Move the LED closer to the sensor")
+    if light > 40000:
+        print("Too bright! Move the LED further away from the sensor")
     time.sleep(0.1)
     
